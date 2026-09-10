cd77307f9a019d1d31ddaa9838b354e03738be2e
Rishi <rishiyelamarthy12345@gmail.com>
2026-09-10 18:38:52 +0530

    Delete primalitytest.py

--- primalitytest.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter value of n "));
-a = int(input("Enter value of a "));
-power = 2**(n-1)
-cal = power%n
-if(cal==1):
-    print("It is a prime");
-
-else:
-    print("It is not a prime");
